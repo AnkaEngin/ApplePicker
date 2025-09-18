90c0c5d75609f74a5efbf1174e86d16b2a4e98f4
AnkaEngin <68354452+AnkaEngin@users.noreply.github.com>
2025-09-17 21:11:17 -0700

    apple falls and collision works

--- Assets/Scripts/Apple.cs
+++ Assets/Scripts/Apple.cs
@@ -1,6 +1,22 @@
 using UnityEngine;
 
-public class Apple
+public class Apple : MonoBehaviour
 {
-    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Apple collided with: " + other.name + " (Tag: " + other.tag + ")");
+        
+        if (other.CompareTag("Basket"))
+        {
+            // Apple caught by basket
+            Debug.Log("Apple caught!");
+            Destroy(gameObject);
+        }
+        else if (other.CompareTag("Ground"))
+        {
+            // Apple hit ground
+            Debug.Log("Apple missed!");
+            Destroy(gameObject);
+        }
+    }
 }
